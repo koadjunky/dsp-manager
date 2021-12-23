81ac0091516589acca5d9e9ff2bad4ad12676c72
Maciej Malycha <koadjunky@gmail.com>
2021-12-23 21:52:51 +0100

    Added factory list

--- fe/src/index.js
+++ fe/src/index.js
@@ -31,7 +31,7 @@ class System extends React.Component {
 class Planet extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {trade: []};
+        this.state = {name: "", trade: [], factories: []};
     }
     componentDidMount() {
         fetch('/dsp/api/stars/1/planets/1')
@@ -42,19 +42,43 @@ class Planet extends React.Component {
         .catch(console.log);
         console.log(this.state);
     }
+    renderFactory(i) {
+        return <Factory factory={this.state.factories[i]} />;
+    }
     render() {
         return (
-            <div className="system">
-                <div className="name">{this.state.name}</div>
-                <div className="title">Imports</div>
-                    {this.state.trade.map(item => item.value < 0 ? <div>{item.name}:{item.value}</div> : "")}
-                <div className="title">Exports</div>
-                    {this.state.trade.map(item => item.value > 0 ? <div>{item.name}:{item.value}</div> : "")}
+            <div>
+                <div className="system">
+                    <div className="name">{this.state.name}</div>
+                    <div className="title">Imports</div>
+                        {this.state.trade.map(item => item.value < 0 ? <div>{item.name}:{item.value}</div> : "")}
+                    <div className="title">Exports</div>
+                        {this.state.trade.map(item => item.value > 0 ? <div>{item.name}:{item.value}</div> : "")}
+                </div>
+                <div className='wrapper'>
+                    {this.state.factories.map(item => <Factory factory={item}/> )}
+                </div>
             </div>
-        )
+        );
     }
 }
 
+class Factory extends React.Component {
+    render() {
+        return (
+            <div className="factory">
+                <div>Name: {this.props.factory.name}</div>
+                <div>Recipe: {this.props.factory.recipe}</div>
+                <div>Machine: {this.props.factory.machine}</div>
+                <div>Count: {this.props.factory.count}</div>
+                <div>Production:</div>
+                {Object.entries(this.props.factory.production).map((key, value) => {
+                    return (<div>{key}: {value}</div>);
+                })}
+            </div>
+        );
+    }
+}
 
 class Interface extends React.Component {
     render() {
