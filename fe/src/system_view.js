2b2d9bcc45e68540e3be81de97c8b53a3433c1c1
Maciej Malycha <koadjunky@gmail.com>
2022-04-04 21:43:56 +0200

    Added system view grid

--- fe/src/system_view.js
+++ fe/src/system_view.js
@@ -1,22 +1,68 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardActionArea from '@mui/material/CardActionArea';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import Typography from '@mui/material/Typography';
+import TableRow from '@mui/material/TableRow';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import { Link } from "react-router-dom";
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
 
+const theme = createTheme();
+
+theme.typography.h6 = {
+    fontSize: '1.0rem'
+};
+
+theme.typography.body1 = {
+    fontSize: '0.8rem'
+};
 class Star extends React.Component {
     render() {
         return (
-            <Link to={`/stars/${this.props.star.name}`}>
-                <div>
-                    <div className="name">{this.props.star.name}</div>
-                    <div className="title">Imports:</div>
-                        {Object.entries(this.props.star.trade).map(([key, value], index) => {
-                            return (value < 0 ? <div>{key}: {value}</div> : "");
-                        })}
-                    <div className="title">Exports:</div>
-                        {Object.entries(this.props.star.trade).map(([key, value], index) => {
-                            return (value > 0 ? <div>{key}: {value}</div> : "");
-                        })}
-                </div>
-            </Link>
+            <ThemeProvider theme={theme}>
+                <Card variant="outlined" sx={{ maxWidth: 240, minWidth: 240, m: 3}}>
+                    <CardActionArea component={Link} to={`/stars/${this.props.star.name}`}>
+                        <CardMedia component="div" image="/images/star.png" sx={{ minHeight: 120, maxHeight: 120 }} alt="Star">
+                            <Typography variant="h6" component="div" color="common.white">{this.props.star.name}</Typography>
+                        </CardMedia>
+                        <CardContent>
+                            <Typography variant="h6" component="div" color="common.white">Imports:</Typography>
+                            <Table size="small">
+                                <TableBody>
+                                    {Object.entries(this.props.star.trade).map(([key, value], index) => {
+                                        return (value < 0 ? 
+                                            <TableRow>
+                                                <TableCell style={{borderBottom: "none"}}>{key}:</TableCell>
+                                                <TableCell align="right" style={{borderBottom: "none"}}>{value}</TableCell>
+                                            </TableRow>
+                                        : "");
+                                    })}
+                                </TableBody>
+                            </Table>
+                            <Typography variant="h6" component="div" color="common.white">Exports:</Typography>
+                            <Table size="small">
+                                <TableBody>
+                                    {Object.entries(this.props.star.trade).map(([key, value], index) => {
+                                        return (value > 0 ? 
+                                            <TableRow>
+                                                <TableCell style={{borderBottom: "none"}}>{key}:</TableCell>
+                                                <TableCell align="right" style={{borderBottom: "none"}}>{value}</TableCell>
+                                            </TableRow>
+                                        : "");
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </ThemeProvider>
         );
     }
 }
@@ -37,9 +83,13 @@ export default class SystemView extends React.Component {
     }
     render() {
         return (
-            <div className="wrapper">
-                {this.state.stars.map(item => <Star key={item.name} star={item}/>)}
-            </div>
-        )
+            <Grid container spacing={1}>
+                <Grid item xs={12}>
+                    <Box sx={{display: 'flex', flexWrap: 'wrap', m: '10'}}>
+                        {this.state.stars.map(item => <Star key={item.name} star={item}/>)}
+                    </Box>
+                </Grid>
+            </Grid>
+        );
     }
 }
